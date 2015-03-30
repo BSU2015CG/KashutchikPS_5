57c7ca13134704c12cf1efea14d8923a25690269
Пётр <fpmkashutch@gmail.com>
2015-03-30 09:16:35 +0300

    Лабораторная работа №2
    Лабораторная работа №1a перемещена в отдельную папку cg_Lab1a

---
+++ cg_Lab2/src/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace cg_Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.txtDPI = new System.Windows.Forms.TextBox();
+            this.txtCompression = new System.Windows.Forms.TextBox();
+            this.txtChrominanceTable = new System.Windows.Forms.TextBox();
+            this.txtLuminanceTable = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png, *.bmp) | *.jpg; *.jpeg; *.jpe; " +
+    "*.jfif; *.png; *.bmp";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(399, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 290);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(106, 4);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(146, 22);
+            this.txtSize.TabIndex = 2;
+            // 
+            // txtDPI
+            // 
+            this.txtDPI.Location = new System.Drawing.Point(106, 32);
+            this.txtDPI.Name = "txtDPI";
+            this.txtDPI.Size = new System.Drawing.Size(146, 22);
+            this.txtDPI.TabIndex = 4;
+            // 
+            // txtCompression
+            // 
+            this.txtCompression.Location = new System.Drawing.Point(106, 60);
+            this.txtCompression.Name = "txtCompression";
+            this.txtCompression.Size = new System.Drawing.Size(146, 22);
+            this.txtCompression.TabIndex = 5;
+            // 
+            // txtChrominanceTable
+            // 
+            this.txtChrominanceTable.Location = new System.Drawing.Point(15, 122);
+            this.txtChrominanceTable.Multiline = true;
+            this.txtChrominanceTable.Name = "txtChrominanceTable";
+            this.txtChrominanceTable.Size = new System.Drawing.Size(181, 142);
+            this.txtChrominanceTable.TabIndex = 6;
+            // 
+            // txtLuminanceTable
+            // 
+            this.txtLuminanceTable.Location = new System.Drawing.Point(202, 122);
+            this.txtLuminanceTable.Multiline = true;
+            this.txtLuminanceTable.Name = "txtLuminanceTable";
+            this.txtLuminanceTable.Size = new System.Drawing.Size(191, 142);
+            this.txtLuminanceTable.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Размер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Разрешение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Сжатие";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quantization Table (Luma)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Quantization Table (Chroma)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 306);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLuminanceTable);
+            this.Controls.Add(this.txtChrominanceTable);
+            this.Controls.Add(this.txtCompression);
+            this.Controls.Add(this.txtDPI);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.TextBox txtDPI;
+        private System.Windows.Forms.TextBox txtCompression;
+        private System.Windows.Forms.TextBox txtChrominanceTable;
+        private System.Windows.Forms.TextBox txtLuminanceTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ cg_Lab2/src/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+
+namespace cg_Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            updatePicture("image.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                updatePicture(openFileDialog1.FileName);
+        }
+
+        private void updatePicture(String fileName)
+        {
+            Image image;
+            try
+            {
+                image = Image.FromFile(fileName);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return;
+            }
+
+            pictureBox1.Image = image;
+
+            PropertyItem[] propItems = image.PropertyItems;
+
+            txtSize.Text = image.PhysicalDimension.Width.ToString() + " * " + image.PhysicalDimension.Height.ToString();
+            txtDPI.Text = image.HorizontalResolution.ToString();
+
+            txtCompression.Text = "no data";
+            foreach (PropertyItem i in propItems)
+                if (i.Id == 0x0103)
+                    txtCompression.Text = i.Value.ToString();
+
+            txtLuminanceTable.Text = "";
+            foreach (PropertyItem property in image.PropertyItems)
+                if (property.Id == 0x5090)
+                    for (int i = 0; i < 8; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                            txtLuminanceTable.Text += String.Format("{0,4:X} ", property.Value[i * 16 + j]);
+                        txtLuminanceTable.Text += Environment.NewLine;
+                    }
+
+            txtChrominanceTable.Text = "";
+            foreach (PropertyItem property in image.PropertyItems)
+                if (property.Id == 0x5091)
+                    for (int i = 0; i < 8; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                            txtChrominanceTable.Text += String.Format("{0,4:X} ", property.Value[i * 16 + j]);
+                        txtChrominanceTable.Text += Environment.NewLine;
+                    }
+        }
+    }
+}
